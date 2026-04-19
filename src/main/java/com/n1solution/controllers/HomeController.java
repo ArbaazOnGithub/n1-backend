8e294f1dbf4139425e67089850e1a591d5bc91eb
ArbaazOnGithub <arbaaz@n1solution.com>
2026-04-19 12:34:27 +0530

    Add HomeController for healthchecks and update db config

---
+++ src/main/java/com/n1solution/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.n1solution.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "N1Solution Backend API is running gracefully!";
+    }
+}
