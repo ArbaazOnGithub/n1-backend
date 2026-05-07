88e4a11c47c601d815be87c281272c4dfad4e6c0
ArbaazOnGithub <arbaaz@n1solution.com>
2026-05-08 00:47:11 +0530

    Optimize: Add Keep-Alive heartbeat and Lazy Initialization for Render

--- src/main/java/com/n1solution/N1SolutionApplication.java
+++ src/main/java/com/n1solution/N1SolutionApplication.java
@@ -3,13 +3,27 @@ package com.n1solution;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class N1SolutionApplication {
+
+	@Value("${RENDER_KEEP_ALIVE:false}")
+	private boolean keepAliveEnabled;
 
 	public static void main(String[] args) {
 		SpringApplication.run(N1SolutionApplication.class, args);
+	}
+
+	@Scheduled(fixedRate = 840000) // 14 minutes in milliseconds
+	public void keepAliveHeartbeat() {
+		if (keepAliveEnabled) {
+			System.out.println("[Heartbeat] Render Keep-Alive: Instance is active at " + java.time.LocalDateTime.now());
+		}
 	}
 
 }
